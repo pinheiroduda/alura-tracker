cf6975ac2ebf261e236d5bf0e69f6bcc18786460
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-18 14:59:08 -0300

    feat: creates Formulario component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateralComponent />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <FormularioComponent />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import BarraLateral from './components/BarraLateral.vue'
+import BarraLateralComponent from './components/BarraLateral.vue'
+import FormularioComponent from './components/Formulario.vue'
 
 export default defineComponent({
   name: 'App',
 
   components: {
-    BarraLateral
+    BarraLateralComponent,
+    FormularioComponent
   }
 })
 </script>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+    </div>
+    <div class="column">
+      <section>
+        <strong>00:00:00</strong>
+      </section>
+      <button class="button">
+        <span class="icon">
+          <i class="fas fa-play"></i>
+        </span>
+        <span>play</span>
+      </button>
+      <button class="button">
+        <span class="icon">
+          <i class="fas fa-stop"></i>
+        </span>
+        <span>stop</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'FormularioComponent'
+})
+</script>
