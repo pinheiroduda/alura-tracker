5ce0715844110abe9d0c8a1143452961637bb91a
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-18 14:47:15 -0300

    feat: creates BarraLateral component

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BarraLateral'
+})
+</script>
+
+<style scoped></style>
