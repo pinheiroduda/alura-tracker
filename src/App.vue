48944d0e821a3f67bae299d9a7b84357c9da7072
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-18 14:48:11 -0300

    feat: creates App style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<template></template>
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter"></div>
+  </main>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import BarraLateral from './components/BarraLateral.vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  components: {
+    BarraLateral
+  }
 })
 </script>
 
-<style></style>
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
