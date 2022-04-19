5fc4318d58f1a9093bc5b68ff38cdf7682043430
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 11:25:21 -0300

    feat: creates 'Tarefa' component

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="column is-7">Descrição da Tarefa</div>
+    <div class="column">
+      <CronometroComponent tempoEmSegundos="15" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CronometroComponent from './Cronometro.vue'
+
+export default defineComponent({
+  name: 'TarefaComponent',
+
+  components: {
+    CronometroComponent
+  }
+})
+</script>
+
+<style scoped>
+.box {
+  background: rgb(250, 195, 79);
+}
+</style>
