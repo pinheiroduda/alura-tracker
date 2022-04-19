2ca9a5c65ee2ed10fdab0d0c9915260291996975
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 11:27:23 -0300

    refactor: displays style that was beeing displayed at App component

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -14,4 +14,17 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
