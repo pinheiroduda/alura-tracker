f44c78ba4c32eb9a7e9f951176ecf978cfe73228
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 09:18:15 -0300

    refactor: form component split data with 'Cronometro' component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -16,9 +16,7 @@
         <div
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
+          <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -39,22 +37,19 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CronometroComponent from './Cronometro.vue'
 
 export default defineComponent({
   name: 'FormularioComponent',
+
+  components: {
+    CronometroComponent
+  },
 
   data() {
     return {
       tempoEmSegundos: 0,
       cronometro: 0
-    }
-  },
-
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000)
-        .toISOString()
-        .substring(11, 19)
     }
   },
 
