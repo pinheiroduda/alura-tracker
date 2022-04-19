34bbceed57979450ec2f974fbeab069f0776f761
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 09:25:38 -0300

    refactor: split data with 'Temporizador' component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -13,23 +13,7 @@
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <TemporizadorComponent />
       </div>
     </div>
   </div>
@@ -37,31 +21,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import CronometroComponent from './Cronometro.vue'
+import TemporizadorComponent from './Temporizador.vue'
 
 export default defineComponent({
   name: 'FormularioComponent',
 
   components: {
-    CronometroComponent
-  },
-
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    }
-  },
-
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1
-      }, 1000)
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    }
+    TemporizadorComponent
   }
 })
 </script>
