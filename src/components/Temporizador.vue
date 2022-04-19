a681ab43103fa05ec9fc287dde54b334f7feebbe
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 09:25:00 -0300

    feat: creates 'Temporizador' component

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CronometroComponent from './Cronometro.vue'
+
+export default defineComponent({
+  name: 'TemporizadorComponent',
+
+  components: {
+    CronometroComponent
+  },
+
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0
+    }
+  },
+
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+    },
+    finalizar() {
+      clearInterval(this.cronometro)
+    }
+  }
+})
+</script>
