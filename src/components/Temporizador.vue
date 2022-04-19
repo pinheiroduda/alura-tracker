61705402247b23883732874f5fe30ac17e977017
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-19 09:32:00 -0300

    feat: adds disabled attribute to set when play and stop button should be able to be clicked

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar">
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finalizar">
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -30,17 +30,20 @@ export default defineComponent({
   data() {
     return {
       tempoEmSegundos: 0,
-      cronometro: 0
+      cronometro: 0,
+      cronometroRodando: false
     }
   },
 
   methods: {
     iniciar() {
+      this.cronometroRodando = true
       this.cronometro = setInterval(() => {
         this.tempoEmSegundos += 1
       }, 1000)
     },
     finalizar() {
+      this.cronometroRodando = false
       clearInterval(this.cronometro)
     }
   }
