9610a111db3d4098a388766dba4c85208ef539a7
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-20 09:09:31 -0300

    feat: creates dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline modo-escuro">
     <div class="column is-one-quarter">
       <BarraLateralComponent />
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
       <FormularioComponent @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
         <TarefaComponent
@@ -60,5 +60,19 @@ export default defineComponent({
 <style scoped>
 .lista {
   padding: 1.25rem;
+}
+
+.main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo {
+  background-color: var(--bg-primario);
 }
 </style>
