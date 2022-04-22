0bcc4ed82b4b941eb3c93f951d685f087abeab5a
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 10:15:30 -0300

    feat: add 'Projetos' setup definition

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -33,27 +33,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import IProjeto from '../interfaces/IProjeto'
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'ProjetosView',
 
   data() {
     return {
-      nomeDoProjeto: '',
-      projetos: [] as IProjeto[]
+      nomeDoProjeto: ''
     }
   },
 
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        id: new Date().toISOString(),
-        nome: this.nomeDoProjeto
-      }
-      this.projetos.push(projeto)
+      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
       this.nomeDoProjeto = ''
+    }
+  },
+  setup() {
+    const store = useStore()
+    return {
+      store,
+      projetos: computed(() => store.state.projetos)
     }
   }
 })
