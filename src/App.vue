b78de459ba337e218f480c3e823102aa25bffad2
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 08:30:08 -0300

    feat: displays content using view router

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,7 @@
       <BarraLateralComponent @aoAlterarTema="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <FormularioComponent @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <TarefaComponent
-          v-for="(tarefa, index) in tarefas"
-          :key="index"
-          :tarefa="tarefa"
-        />
-        <BoxComponent v-if="listaEstaVazia">
-          Ainda não foi registrada nenhuma tarefa hoje:/
-        </BoxComponent>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -25,42 +15,24 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import BarraLateralComponent from './components/BarraLateral.vue'
-import FormularioComponent from './components/Formulario.vue'
-import TarefaComponent from './components/Tarefa.vue'
-import BoxComponent from './components/Box.vue'
-import ITarefa from './interfaces/ITarefa'
 
 export default defineComponent({
   name: 'App',
 
+  components: {
+    BarraLateralComponent
+  },
+
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
 
-  computed: {
-    listaEstaVazia(): boolean {
-      return this.tarefas.length === 0
-    }
-  },
-
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
-
     trocarTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
-  },
-
-  components: {
-    BarraLateralComponent,
-    FormularioComponent,
-    TarefaComponent,
-    BoxComponent
   }
 })
 </script>
