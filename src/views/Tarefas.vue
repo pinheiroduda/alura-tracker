a6b4a080507e7e7ecac41fe50be51f5a6ea3ca98
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 08:28:58 -0300

    feat: creates 'Tarefas' view

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,49 @@
+<template>
+  <FormularioComponent @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <TarefaComponent
+      v-for="(tarefa, index) in tarefas"
+      :key="index"
+      :tarefa="tarefa"
+    />
+    <BoxComponent v-if="listaEstaVazia">
+      Ainda não foi registrada nenhuma tarefa hoje:/
+    </BoxComponent>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FormularioComponent from '../components/Formulario.vue'
+import TarefaComponent from '../components/Tarefa.vue'
+import BoxComponent from '../components/Box.vue'
+import ITarefa from '../interfaces/ITarefa'
+
+export default defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      tarefas: [] as ITarefa[]
+    }
+  },
+
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    }
+  },
+
+  components: {
+    FormularioComponent,
+    TarefaComponent,
+    BoxComponent
+  }
+})
+</script>
