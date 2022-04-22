82090b7040ad183510a07cf8be10d0ff87336387
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 13:32:11 -0300

    feat: displays notification according to each type

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,25 +1,41 @@
 <template>
   <div class="notificacoes">
-    <article class="message is-success">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto</div>
-    </article>
-    <article class="message is-warning">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto</div>
-    </article>
-    <article class="message is-danger">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Texto</div>
+    <article
+      class="message"
+      :class="contexto[notificacao.tipo]"
+      v-for="notificacao in notificacoes"
+      :key="notificacao.id"
+    >
+      <div class="message-header">{{ notificacao.titulo }}</div>
+      <div class="message-body">{{ notificacao.texto }}</div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { TipoNotificacao } from '@/interfaces/INotificacao'
+import { useStore } from '@/store'
+import { computed, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'NotificacoesComponent'
+  name: 'NotificacoesComponent',
+
+  data() {
+    return {
+      contexto: {
+        [TipoNotificacao.SUCESSO]: 'is-success',
+        [TipoNotificacao.ATENCAO]: 'is-warning',
+        [TipoNotificacao.FALHA]: 'is-danger'
+      }
+    }
+  },
+
+  setup() {
+    const store = useStore()
+    return {
+      notificacoes: computed(() => store.state.notificacoes)
+    }
+  }
 })
 </script>
 
