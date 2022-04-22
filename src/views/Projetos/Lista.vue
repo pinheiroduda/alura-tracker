b1cd37239ff4bcc0cc79297d56c4f4585bd6cd10
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 11:21:21 -0300

    refactor: pass content of view 'Projeto' to the new view 'Lista'

---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store'
+
+export default defineComponent({
+  name: 'ListaView',
+
+  setup() {
+    const store = useStore()
+    return {
+      projetos: computed(() => store.state.projetos)
+    }
+  }
+})
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
