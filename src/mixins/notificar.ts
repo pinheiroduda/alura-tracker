f627b96d57e6157a1d5c180ab75de2996e3a2ae3
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 14:16:01 -0300

    fix: adds void return to function

--- src/mixins/notificar.ts
+++ src/mixins/notificar.ts
@@ -4,7 +4,7 @@ import {store} from '@/store'
 
 export const notificacaoMixin = {
   methods: {
-    notificar(tipo: TipoNotificacao, titulo: string, texto: string) {
+    notificar(tipo: TipoNotificacao, titulo: string, texto: string) : void {
       store.commit(NOTIFICA, {
         tipo,
         titulo,
