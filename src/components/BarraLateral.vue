d61a368eaa0ca5be6e480e05ba834afaaab8798d
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 09:03:24 -0300

    feat: creates navigation menu to app routes

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,9 +1,25 @@
 <template>
   <header>
     <h1>
-      <img src="../assets/logo.png" alt="" />
+      <img src="../assets/logo.png" alt="Alura Tracker" />
     </h1>
-    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks">tarefas</i>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram">projetos</i>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -40,17 +56,34 @@ export default defineComponent({
 </script>
 
 <style scoped>
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
 header {
-  padding: 1rem;
+  padding: 2rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
 }
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
