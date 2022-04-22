98c89fe9bab50c7e8d25e6f1b0a6393b7fe1cdbb
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-22 09:18:27 -0300

    feat: creates 'Projetos' view state and method to receive and record input data

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>Projetos</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button type="submit" class="button">Salvar</button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import IProjeto from '../interfaces/IProjeto'
 
 export default defineComponent({
-  name: 'ProjetosView'
+  name: 'ProjetosView',
+
+  data() {
+    return {
+      nomeDoProjeto: ''
+    }
+  },
+
+  methods: {
+    salvar() {
+      const projeto: IProjeto = {
+        id: new Date().toISOString(),
+        nome: this.nomeDoProjeto
+      }
+    }
+  }
 })
 </script>
