5caf312a23d7a2e932220b5e2befa23504e8d9b5
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-25 13:59:29 -0300

    refactor: modal text

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -14,15 +14,15 @@
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Modal title</p>
+          <p class="modal-card-title">Edite sua tarefa</p>
           <button class="delete" aria-label="close"></button>
         </header>
         <section class="modal-card-body">
           <!-- Content ... -->
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success">Save changes</button>
-          <button class="button">Cancel</button>
+          <button class="button is-success">Salvar alterações</button>
+          <button class="button">Cancelar</button>
         </footer>
       </div>
     </div>
